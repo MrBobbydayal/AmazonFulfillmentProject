bcd10ac6189ceae12177dc240b7e892c382564e9
MrBobbydayal <bobbydayal398@gmail.com>
2026-01-16 01:19:46 +0530

    commit correct sql ddl queries

--- database/01_schema.sql
+++ database/01_schema.sql
@@ -1,8 +1,45 @@
-SELECT TOP 5
-    o.order_id,
-    w.region,
-    s.delivery_time,
-    s.cost
-FROM Orders o
-JOIN Warehouse w ON o.warehouse_id = w.warehouse_id
-JOIN Shipment s ON o.order_id = s.order_id;
+CREATE DATABASE amazon_fulfillment;
+GO 
+USE amazon_fulfillment; 
+GO 
+
+-- Warehouse 
+CREATE TABLE Warehouse ( warehouse_id INT IDENTITY PRIMARY KEY,
+                         region VARCHAR(50) NOT NULL,
+                          capacity INT NOT NULL, 
+                          manager VARCHAR(50) NOT NULL );
+                          
+ -- Inventory 
+ CREATE TABLE Inventory ( inventory_id INT IDENTITY PRIMARY KEY, 
+                         item_name VARCHAR(50) NOT NULL,
+                          stock_level INT NOT NULL,
+                           warehouse_id INT NOT NULL,
+                            FOREIGN KEY (warehouse_id) REFERENCES Warehouse(warehouse_id) );
+                            
+                            
+ -- Orders 
+ CREATE TABLE Orders ( order_id INT IDENTITY PRIMARY KEY, 
+                       customer_id INT NOT NULL,
+                        warehouse_id INT NOT NULL,
+                         order_date DATE NOT NULL,
+                          delivery_date DATE NOT NULL,
+                           status VARCHAR(20) NOT NULL,
+                            FOREIGN KEY (warehouse_id) REFERENCES Warehouse(warehouse_id) ); 
+                            
+                            
+-- Shipment 
+CREATE TABLE Shipment ( shipment_id INT IDENTITY PRIMARY KEY,
+                        order_id INT NOT NULL,
+                         cost FLOAT NOT NULL,
+                          delay_reason VARCHAR(50) DEFAULT 'None',
+                           delivery_time INT NOT NULL,
+                            FOREIGN KEY (order_id) REFERENCES Orders(order_id) ); 
+                            
+-- Cost Metrics 
+CREATE TABLE CostMetrics ( cost_id INT IDENTITY PRIMARY KEY,
+                           warehouse_id INT NOT NULL,
+                            date DATE NOT NULL,
+                             labor_hours FLOAT NOT NULL, 
+                             transport_cost FLOAT NOT NULL,
+                              total_cost FLOAT NOT NULL,
+                               FOREIGN KEY (warehouse_id) REFERENCES Warehouse(warehouse_id) );
